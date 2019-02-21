2ed919aea1eb8b80d7094875fa22bfd8d3c7db12
Tory Dannenberg <rfdberg@gmail.com>
2019-02-21 10:52:39 -0700

    Add NProgress bar for loading between pages

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import Nav from './Nav.js';
 import Link from 'next/link';
 import styled from 'styled-components';
+import Nav from './Nav.js';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+}
+Router.onRouteChangeError = () => {
+  NProgress.done();
+}
 
 const Logo = styled.h1`
   font-size: 4rem;

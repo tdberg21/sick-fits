058c8b9ddaf974ba7c5982f73cc82e8350b94a03
Tory Dannenberg <rfdberg@gmail.com>
2019-03-04 11:32:37 -0700

    Render Item component for each item that is returned from query

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Item from '../components/Item';
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -15,12 +17,34 @@ const ALL_ITEMS_QUERY = gql`
   }
 `;
 
-export default class Items extends Component {
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+`;
+
+class Items extends Component {
   render() {
     return (
-      <div>
-        <p>Items!!</p>
-      </div>
+      <Center>
+        <Query query={ALL_ITEMS_QUERY}>
+          {({data, error, loading}) => {
+            if (loading) return <p>Loading...</p>
+            if(error) return <p>Error: {error.message}</p>
+            return <ItemsList>
+              {data.items.map(item => {
+              return <Item item={item} key={item.id}/>})}
+            </ItemsList>;
+          }}
+        </Query>
+      </Center>
     );
   }
 }
+
+export default Items;
